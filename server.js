e9f7437661bdaf8bfc315067dafaab1fdeb08a51
Søren Markersen <soren@Srens-MacBook-Air.local>
2019-03-21 15:22:43 +0100

    Sørens commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send('Hello everyone')
 })
 
 app.listen(9000, () => console.log('Listening http://localhost' + 9000))
